50e6eae2145ce0129cc894a658b6bd77ad954c97
BrsPontes-Vortx <brb@vortx.com.br>
2021-02-05 19:57:13 -0300

    trabalhando com arrays

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ const typeDefs = gql`
 
   scalar Date
 
+  type Produto {
+    nome: String!
+    preco: Float!
+    desconto: Float
+    precoComDesconto: Float
+  }
+
   type Usuario {
     id: ID!
     nome: String!
     email: String!
-    Idade: Int
-    Salario: Float
+    idade: Int
+    salario: Float
     vip: Boolean
   }
 
@@ -18,10 +25,26 @@ const typeDefs = gql`
     ola: String
     horaAtual: Date
     usuarioLogado: Usuario
+    produtoEmDestaque: Produto
+    numerosMegaSena: [Int!]!
   }
 `
 
 const resolvers = { 
+  Produto: {
+    precoComDesconto(produto) {
+      if(produto.desconto) {
+        return produto.preco * (1 - produto.desconto)
+      } else {
+        return produto.preco
+      }
+    }
+  },
+  Usuario: {
+    salario(usuario) {
+      return usuario.salario_real
+    }
+  },
   Query: {
     ola () {
       return 'Qualquer retorno'
@@ -35,9 +58,20 @@ const resolvers = {
         nome: 'Ana',
         email: 'ana@web.com',
         idade: 23,
-        salario: 1234.56,
+        salario_real: 1234.56,
         vip: true
       }
+    },
+    produtoEmDestaque() {
+      return {
+        nome: 'Notebook Gamer',
+        preco: 4890.89,
+        desconto: 0.15
+      }
+    },
+    numerosMegaSena() {
+      const crescente =  (a, b) => a - b
+      return Array(6).fill(0).map(n => parseInt(Math.random() * 60 + 1)).sort(crescente)
     }
   }
 }
